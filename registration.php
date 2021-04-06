1bec55aa63ffdcd439ed714fa51ae0c8a57df08c
joshuamakula <makulajosh@gmail.com>
2021-04-06 16:18:49 +0300

    Create SQL queries.

--- registration.php
+++ registration.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
 
-    $con = mysqli_connect('localhost', 'root', '123456789');
+    $con = mysqli_connect('127.0.0.1', 'root', '123456789');
     mysqli_select_db($con, 'tutorial');
 
     $name = $_POST['user'];
     $pass = $_POST['password'];
 
-    $s = "SELECT * FROM usertable WHERE name = '$nam]";
+    $s = "SELECT * FROM usertable WHERE name = '$name'";
 
     $result = mysqli_query($con, $s);
     $num = mysqli_num_rows($result);
