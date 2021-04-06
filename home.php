37cf242dc489f376fc7e7d852649ea4337f79f08
joshuamakula <makulajosh@gmail.com>
2021-04-06 17:11:12 +0300

    Create and destroy sessions

---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header('location:login.php');
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+
+         <a href="logout.php">LOGOUT</a>
+
+        <h1>Welcome <?php echo $_SESSION['username'] ?> </h1>
+    </div>
+   
+
+</body>
+</html>
--- validation.php
+++ validation.php
@@ -2,26 +2,22 @@
     // start session
     session_start();
 
-    // Redirect user
-    header('location:login.php');
-
     $con = mysqli_connect('127.0.0.1', 'root', '123456789');
     mysqli_select_db($con, 'tutorial');
 
     $name = $_POST['user'];
     $pass = $_POST['password'];
 
-    $s = "SELECT * FROM usertable WHERE name = '$name'";
+    $s = "SELECT * FROM usertable WHERE name = '$name' && password = '$pass' ";
 
     $result = mysqli_query($con, $s);
     $num = mysqli_num_rows($result);
 
     if($num == 1){
-        echo "Username Already Taken";
+        $_SESSION['username'] = $name;
+        header('location:home.php');
     }else{
-        $reg = "INSERT INTO usertable(name, password) VALUES ('$name', '$pass')";
-        mysqli_query($con, $reg); 
-        echo "Registration Successful";
+        header('location:login.php');
     }
 
-?>
+?> 
