c7a640fc01200052a9a1f2a37fb440ff9e6aa0b2
joshuamakula <makulajosh@gmail.com>
2021-04-06 13:14:52 +0300

    Add CSS to the page.

--- login.php
+++ login.php
@@ -5,40 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Login and Registration</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h2>Login Here</h2>
-                <form action="validation.php" method="post">
-                    <div class="form-group">
-                        <label for="">Username</label>
-                        <input type="text" name="name" id="name" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
+        <div class="login-box">
+            <div class="row">
+                <div class="col-md-6 login-left">
+                    <h2>Login Here</h2>
+                    <form action="validation.php" method="post">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="name" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+                <div class="col-md-6 login-right">
+                    <h2>Register Here</h2>
+                    <form action="registration.php" method="post">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="name" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Register</button>
+                    </form>
+                </div>
+                
             </div>
-            <div class="col-md-6">
-                <h2>Register Here</h2>
-                <form action="validation.php" method="post">
-                    <div class="form-group">
-                        <label for="">Username</label>
-                        <input type="text" name="name" id="name" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
-                </form>
-            </div>
-            
         </div>
     </div>
     
